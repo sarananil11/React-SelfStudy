88415c09f437df295da34233add38f5b8b50293c
sarananil11 <sarananilvp2020@gmail.com>
2025-10-27 21:51:46 +0530

    learned basics

---
+++ vite-project/src/Footer.jsx
@@ -0,0 +1,14 @@
+
+function Footer() {
+
+    return(
+        <footer>
+            <p>
+                &copy; {new Date().getFullYear()} Your website name
+            </p>
+        </footer>
+    );
+
+}
+
+export default Footer
